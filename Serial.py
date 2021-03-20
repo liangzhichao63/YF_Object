f01f428294d06724352c9f22051711669b0fb52b
liangzhichao63 <935838323@qq.com>
2021-03-20 19:52:02 +0800

    Serial update

--- Serial.py
+++ Serial.py
@@ -6,36 +6,101 @@ bps= 115200
 timex=5 
 ser = serial.Serial(portx, int(bps), timeout=1, parity=serial.PARITY_NONE,stopbits=1) 
 if (ser.isOpen()): 
-    print("open success") 
-    # os.system('minicom &')
+    print("串口打开成功") 
+else :
+    print("请检查串口是否链接或用sudo运行")
 
 
-while True:
+def Read_Data():
     line = ser.readline()
     print(line)
-    time.sleep(0.01)
+
+def Send_Data(data):
+    global ser
+    ser.write(data)
+
+data=bytearray(5)
+data[0]=0xff
+data[1]=0xfe
+
+def Protocol():
+    global data 
+    data[4]=(data[2]+data[3])%256
+    ser.write(data)
 
 
-    
-def posiion_get():
-    ser = serial.Serial(portx, int(bps), timeout=1, parity=serial.PARITY_NONE,stopbits=1) 
-    if (ser.isOpen()): 
-        print("open success") 
-        os.system('minicom &')
-        #ser.write("hello".encode()) 
-       
-        line = ser.readline() 
-        if(line): 
-            #print(str(line) )
-            temp = str(line)
-            temp = temp.split('=')
-            #print(temp)
-            temp_x_y=[]
-            for i in [1,2]:
-                tempq=temp[i]
-                tempq=tempq.split('C')
-                tempq=int(tempq[0])
-                temp_x_y.append(tempq)
-                #print(temp_x_y)
-            line=0 
-        return temp_x_y
+# while True:
+#     data[2]=0xA1
+#     data[3]=0xEA
+
+#     Protocol()
+#     time.sleep(1)
+#     Read_Data()
+#define _PS2_Key_Dir_U  0x0010
+#define _PS2_Key_Dir_D  0x0040
+#define _PS2_Key_Dir_L  0x0080
+#define _PS2_Key_Dir_R  0x0020
+
+#define _PS2_Key_Butt_U  0x1000
+#define _PS2_Key_Butt_D  0x4000
+#define _PS2_Key_Butt_L  0x8000
+#define _PS2_Key_Butt_R  0x2000
+
+#define _PS2_Key_SideL_1 0x0400
+#define _PS2_Key_SideL_2 0x0100
+
+#define _PS2_Key_SideR_1 0x0800
+#define _PS2_Key_SideR_2 0x0200
+
+#define _PS2_Key_SELECT  0x0001
+#define _PS2_Key_MODE    0x0008
+
+
+
+def Run():
+    global data
+    data[2]=0x00
+    data[3]=0x10
+    Protocol()
+
+def Back():
+    global data
+    data[2]=0x00
+    data[3]=0x40
+    Protocol()
+
+def Left():
+    global data
+    data[2]=0x00
+    data[3]=0x80
+    Protocol()
+
+def Right():
+    global data
+    data[2]=0x00
+    data[3]=0x20
+    Protocol()
+
+def Across_Left():
+    global data
+    data[2]=0x00
+    data[3]=0x01
+    Protocol()
+
+def Across_Right():
+    global data
+    data[2]=0x00
+    data[3]=0x08
+    Protocol()
+
+# def Right():
+#     global data
+#     data[2]=0x00
+#     data[3]=0x20
+#     Protocol()
+
+# def Right():
+#     global data
+#     data[2]=0x00
+#     data[3]=0x20
+#     Protocol()
